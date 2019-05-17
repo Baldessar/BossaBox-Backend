35edfccb1b779194b538bb2439470a2a0d1fa6e5
Baldessar <gb.baldessar@gmail.com>
2019-05-16 21:32:36 -0300

    add tools router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000 
 
+/* Routes */
+const tools = require("./tools/routes.js")
+
+
+app.use("/",tools)
+
 app.listen(port, function () {
   console.log(`listening on port ${port}!`);
 });
