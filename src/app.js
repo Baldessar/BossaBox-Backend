7e954d2c1711d4be56b4d654cec2c43bce0105ce
Baldessar <gb.baldessar@gmail.com>
2019-05-19 11:38:13 -0300

    add morgan and error middleware

--- src/app.js
+++ src/app.js
@@ -1,12 +1,43 @@
-const express = require('express');
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+import mongoose from "mongoose"
+import express from "express"
+import morgan from "morgan"
+import "./mongoose/connection"
+
 const app = express();
-const port = 3000 
 
 /* Routes */
-const tools = require("./tools/routes.js")
+import tools from "./tools/routes"
+mongoose.connect('mongodb://localhost:27017/bossabox', { useNewUrlParser: true });
 
+app.use(morgan("tiny")) // HTTP request logger (can be safely disabled if wanted)
+app.use(express.json()) // Parses requests to JSON format
 
-app.use("/",tools)
+const port = 3000
+app.use("/", tools)
+
+app.use((err, req, res, next) => {
+  console.error(err)
+
+  // Errors without a defined reason
+  if (!err.status) {
+    err.status = 500
+  } else {
+    res.status(err.status)
+  }
+
+  if (!err.reason) {
+    return res.end()
+  }
+
+  // Errors with a defined reason
+
+  return res.json({
+    error: err.reason,
+  })
+})
+
 
 app.listen(port, function () {
   console.log(`listening on port ${port}!`);
