814e37a7dce4255d216174c12bf8e7cb2e0c661f
Baldessar <gb.baldessar@gmail.com>
2019-05-27 18:48:11 -0300

    add docker filess

--- src/tools/toolManager.js
+++ src/tools/toolManager.js
@@ -1,43 +1,42 @@
 import mongoose from "mongoose"
 import "../mongoose/tools"
 
-const Tools = mongoose.model('tools');
-
+const Tools = mongoose.model("tools")
 
 export const getTools = async ({ tag }) => {
-    try {
-        const filter = tag ? { tags: tag } : undefined
-        const tools = await Tools.find(filter)
-        return tools
-    } catch (error) {
-        throw error
-    }
+  try {
+    const filter = tag ? { tags: tag } : undefined
+    const tools = await Tools.find(filter)
+    return tools
+  } catch (error) {
+    throw error
+  }
 }
 
 export const insertTool = async ({ title, link, description, tags }) => {
-    try {
-        const newTool = new Tools({ title, link, description, tags })
-        const tool = await newTool.save()
-        return tool.toObject()
-    } catch (error) {
-        throw error
-    }
+  try {
+    const newTool = new Tools({ title, link, description, tags })
+    const tool = await newTool.save()
+    return tool.toObject()
+  } catch (error) {
+    throw error
+  }
 }
 
-export const deleteTool = async (toolid) => {
-    try {
-        const oldTool = await Tools.findByIdAndDelete(toolid)
-        return oldTool
-    } catch (error) {
-        throw error
-    }
+export const deleteTool = async toolid => {
+  try {
+    const oldTool = await Tools.findByIdAndDelete(toolid)
+    return oldTool
+  } catch (error) {
+    throw error
+  }
 }
 
-export const findById = async (toolId) => {
-    try {
-        const selectedTool = await Tools.findById(toolId)
-        return selectedTool.toObject()
-    } catch (error) {
-        throw error
-    }
+export const findById = async toolId => {
+  try {
+    const selectedTool = await Tools.findById(toolId)
+    return selectedTool.toObject()
+  } catch (error) {
+    throw error
+  }
 }
